9f886c9f07af48ecc554c83e2b26a323994db103
Nathan Cho <ntcho13@gmail.com>
2022-10-24 16:27:52 -0400

    📦 Add ML Assignment 2

---
+++ Fall 2022/CMSC 352 – Machine Learning/Assignment 2/problem2.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+import sklearn.metrics
+
+def read_data(filename):
+    #reads in a csv and segments the data
+    #randomizes the order of the data, then splits it into different sets
+    #returns separate inputs (x) and outputs (y) for each of training, test, and validation
+    #also returns a list of column names, which may be useful for determining heavily weighted features
+    df = pd.read_csv(filename)
+    data = df.to_numpy()
+    np.random.shuffle(data)
+    test_size = int(data.shape[0]/10)
+    data_test = data[:test_size]
+    data_val = data[test_size:2*test_size]
+    data_train = data[2*test_size:]
+    x_train = data_train[:,1:]
+    y_train = data_train[:,0]
+    x_val = data_val[:,1:]
+    y_val = data_val[:,0]
+    x_test = data_test[:,1:]
+    y_test = data_test[:,0]
+    return x_train, y_train, x_val, y_val, x_test, y_test, df.columns.values
+
+def train_logistic_regression(x_train, y_train, x_val, y_val, lr):
+    x_train = add_ones(x_train)
+    weights = np.zeros(x_train.shape[1])
+    
+    prev_weights = weights
+    prev_accuracy = 0.0 # higher the better
+    iteration = 1
+    
+    while True:
+        for i in range(len(x_train)):
+            deriv = (y_train[i] - compute_hypothesis(x_train[i], weights)) * x_train[i]
+            weights = weights + lr * deriv
+        
+        print(f"Train iteration {iteration}:")
+        accuracy, precision, recall = evaluate_logistic_regression(x_val, y_val, weights)
+        
+        if accuracy <= prev_accuracy:
+            weights = prev_weights # revert to previous weights
+            break
+        
+        prev_weights = weights
+        prev_accuracy = accuracy
+        iteration = iteration + 1
+    
+    return weights
+
+def add_ones(x):
+    #takes an array of feature vectors and adds a column of 1s to the start
+    #useful for logistic regression, since x_0 is always 1
+    return np.insert(x, 0, np.ones(x.shape[0]), axis = 1)
+
+def compute_hypothesis(x, weights):
+    #computes the hypothesis function for logistic function given data x and weights
+    #if x is a single feature vector, will return a scalar
+    #if x is a matrix of feature vectors, will return a vector containing the hypothesis for each row
+    prod = x.dot(weights)
+    return 1/(1 + np.exp(-prod))
+
+def evaluate_logistic_regression(x, y, weights):
+    x = add_ones(x)
+    #takes in a matrix of feature vectors x, a vector of class labels y
+    #also takes a vector weights for the classifier
+    #returns the error on the data (x, y) of the logistic regression classifier
+    y_pred = (compute_hypothesis(x, weights) > 0.5).astype(float)
+    
+    accuracy = sklearn.metrics.accuracy_score(y, y_pred)
+    precision = sklearn.metrics.precision_score(y, y_pred, average="macro", zero_division=0)
+    recall = sklearn.metrics.recall_score(y, y_pred, average="macro", zero_division=0)
+    
+    print(f"  accuracy={accuracy:.5f}, \n  precision={precision:.5f} \n  recall={recall:.5f}")
+    return accuracy, precision, recall
+
+def rank_features(weights, feats):
+    #takes in a weight vector and an array of feature names
+    #returns a sorted array of features, sorted from most negatively weighted to most positively weighted
+    #note that feats MUST be a numpy array of the same length as weights
+    #if feats[i] does not correspond to weights[i], this will not return accurate results
+    imp = np.argsort(weights)
+    return list(zip(feats[imp], weights[imp]))
+
+def test_logistic_regression():
+    x_train, y_train, x_val, y_val, x_test, y_test, feats = read_data("mushrooms_logistic.csv")
+    lr = 0.00001  # learning rate
+
+    # train logistic regression
+    weights = train_logistic_regression(x_train, y_train, x_val, y_val, lr)
+    print(f"Weights: {weights}")
+
+    # test logistic regression
+    print(f"Training set:")
+    evaluate_logistic_regression(x_train, y_train, weights)
+
+    print(f"Validation set:")
+    evaluate_logistic_regression(x_val, y_val, weights)
+
+    print(f"Test set:")
+    accuracy, precision, recall = evaluate_logistic_regression(x_test, y_test, weights)
+
+    ranked_feats = rank_features(weights, feats)
+    print(f"Top 3 indicative feats for edible mushrooms (y = 0):\n  {ranked_feats[:3]}")
+    print(f"Top 3 indicative feats for poisionous mushrooms (y = 1):\n  {ranked_feats[::-1][:3]}")
+    print()
+    
+    return weights, accuracy, precision, recall
+
+x_train, y_train, x_val, y_val, x_test, y_test, feats = read_data("mushrooms_perceptron.csv")
+
+iteration = 1
+if iteration == 1:
+    # measure individual performance
+    test_logistic_regression()
+else:
+    # measure average performance
+    weights_sum = np.zeros(x_train.shape[1] + 1)
+    accuracy_sum = 0
+    precision_sum = 0
+    recall_sum = 0
+
+    for i in range(iteration):
+        print(f">> Test iteration {i}:")
+        weights, accuracy, precision, recall = test_logistic_regression()
+        weights_sum += weights
+        accuracy_sum += accuracy
+        precision_sum += precision
+        recall_sum += recall
+
+    print(f"Average weights: {weights_sum / iteration}")
+    print(f"Average test accuracy: {accuracy_sum / iteration:.5f}")
+    print(f"Average test precision: {precision_sum / iteration:.5f}")
+    print(f"Average test recall: {recall_sum / iteration:.5f}")
+
+    ranked_feats = rank_features(weights_sum / iteration, feats)
+    print(f"Average top 3 indicative feats for edible mushrooms (y = 0):\n  {ranked_feats[:3]}")
+    print(f"Average top 3 indicative feats for poisionous mushrooms (y = 1):\n  {ranked_feats[::-1][:3]}")
