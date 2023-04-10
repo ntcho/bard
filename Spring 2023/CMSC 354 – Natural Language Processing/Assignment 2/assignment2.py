c72f0fb867289f16d5e825ae1d702bc9cde58101
Nathan Cho <ntcho13@gmail.com>
2023-04-10 15:57:12 -0400

    🚑 Optimize NLP Assignment 2

--- Spring 2023/CMSC 354 – Natural Language Processing/Assignment 2/assignment2.py
+++ Spring 2023/CMSC 354 – Natural Language Processing/Assignment 2/assignment2.py
@@ -6,19 +6,19 @@ from tqdm import tqdm
 print("training: reading training data")
 
 # Read the csv file containing the training data
-df = pd.read_csv('IMDB_train.csv')
+df = pd.read_csv("IMDB_train.csv")
 
 # Convert all review strings into lowercase
-df['review'] = df['review'].apply(str.lower)
+df["review"] = df["review"].apply(str.lower)
 
 # Tokenize the review string into individual words
-df['review'] = df['review'].apply(str.split)
+df["review"] = df["review"].apply(str.split)
 
 
 print("training: reading vocabulary data")
 
 # Create a list of vocabulary from the "imdb.vocab" file
-with open('imdb.vocab', 'r') as f:
+with open("imdb.vocab", "r") as f:
     vocab = f.read().splitlines()
 
 print("training: calculating prior probabilities")
@@ -27,79 +27,103 @@ print("training: calculating prior probabilities")
 pos_prob = {}
 neg_prob = {}
 
-pos_count = len(df[df['sentiment'] == 'pos'])
-neg_count = len(df[df['sentiment'] == 'neg'])
+# Create two dictionaries to store positive/negative occurence with word as key
+pos_count = {word: 0 for word in vocab}
+neg_count = {word: 0 for word in vocab}
+
+pos_count_all = len(df[df["sentiment"] == "pos"])
+neg_count_all = len(df[df["sentiment"] == "neg"])
 
 # Calculate the prior probabilities of positive and negative classes
-pos_prior = pos_count / len(df)
-neg_prior = neg_count / len(df)
+pos_prior = pos_count_all / len(df)
+neg_prior = neg_count_all / len(df)
 
 
-print("training: calculating likelihood probabilities")
+print("training: calculating likelihood probabilities (iterating through all reviews)")
 
-# Calculate the likelihoods of each word given the positive and negative classes
+# Helper function to optimize performance
+def count_pos(word):
+    pos_count[word] += 1
+
+def count_neg(word):
+    neg_count[word] += 1
+
+
+for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+    # Defines which operation to perform according to the label
+    count = count_pos if row["sentiment"] == "pos" else count_neg
+
+    for word in row["review"]:
+        try:
+            # Count the number of reviews containing the word
+            count(word)
+        except:
+            # Ignore words that are not in the vocabulary
+            continue
+        
+        
+print("training: calculating likelihood probabilities (iterating through all vocabulary words)")
+
 for word in tqdm(vocab):
-    # Count the number of reviews containing the word
-    word_sentiments = df[(df['review'].str.contains(word, regex=False))]['sentiment']
-    
-    # Count the number of positive and negative reviews
-    word_pos_count = word_sentiments.str.count("pos").sum()
-    word_neg_count = len(word_sentiments) - word_pos_count # optimization instead of 'neg'
-    
     # Calculate the probabilities using Laplace add-one smoothing
-    pos_prob[word] = (word_pos_count + 1) / (pos_count + len(vocab))
-    neg_prob[word] = (word_neg_count + 1) / (neg_count + len(vocab))
+    pos_prob[word] = (pos_count[word] + 1) / (pos_count_all + len(vocab))
+    neg_prob[word] = (neg_count[word] + 1) / (neg_count_all + len(vocab))
+
 
 def predict_sentiment(review):
     """
     Predicts the sentiment of a review using the Naive Bayes model.
-    
+
     Args:
         review: A string containing the text of the review.
-    
+
     Returns:
         A string representing the predicted sentiment ('pos' or 'neg').
     """
     # Tokenize the review into individual words
     words = review.split()
-    
+
     # Initialize the probabilities of positive and negative classes
-    pos_p = 0 if pos_prior == 0 else log2(pos_prior)
-    neg_p = 0 if neg_prior == 0 else log2(neg_prior)
-    
+    pos_p = log2(pos_prior)
+    neg_p = log2(neg_prior)
+
     # Calculate the probabilities by adding the log likelihoods of each word
     for word in words:
-        if word in vocab:
+        try:
             pos_p += log2(pos_prob[word])
             neg_p += log2(neg_prob[word])
-    
+        except:
+            # Ignore words that are not in the vocabulary
+            continue
+
     # Return the prediction based on which probability is higher
     if pos_p > neg_p:
-        return 'pos'
+        return "pos"
     else:
-        return 'neg'
+        return "neg"
+
 
 def evaluate(test_df):
     """
     Evaluates the performance of the Naive Bayes model on a test set.
-    
+
     Args:
         test_df: A pandas DataFrame containing the test data.
-    
+
     Returns:
         A tuple containing the accuracy, precision, and recall of the model on the test set.
     """
-    
+
     tqdm.pandas()
-    
+
     # Make predictions for each review in the test set
-    y_pred = test_df['review'].progress_apply(predict_sentiment)
-    
+    y_pred = test_df["review"].progress_apply(predict_sentiment)
+
     # Calculate accuracy, precision, and recall
-    accuracy = accuracy_score(test_df['sentiment'], y_pred)
-    precision = precision_score(test_df['sentiment'], y_pred, pos_label='pos')
-    recall = recall_score(test_df['sentiment'], y_pred, pos_label='pos')
-    
+    accuracy = accuracy_score(test_df["sentiment"], y_pred)
+    precision = precision_score(test_df["sentiment"], y_pred, pos_label="pos")
+    recall = recall_score(test_df["sentiment"], y_pred, pos_label="pos")
+
     # Return the results
     return accuracy, precision, recall
 
@@ -108,7 +132,7 @@ print()
 print("testing: reading testing data")
 
 # Read the csv file containing the test data
-test_df = pd.read_csv('IMDB_test.csv')
+test_df = pd.read_csv("IMDB_test.csv")
 
 
 print("testing: evaluating model performance")
